ac5552eba3fda8cbf23809f3a2730e76f227bfee
mancjames <multicolouredsmurf@gmail.com>
2021-01-10 13:40:00 +0000

    added validation requirements for first name/lastname/email in html by adding required and added functionality to close button on form

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModal = document.getElementById('close');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +20,11 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+//close form
+
+closeModal.addEventListener("click", () => {
+  modalbg.style.display = "none";
+})
 
 
